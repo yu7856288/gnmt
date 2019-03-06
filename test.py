c04826d7195a020f57915fb4d9b74db37befc1bb
yu7856288 <732626736@qq.com>
2019-03-06 16:59:03 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+
+websites = [
+    ('Sohu', 'http://www.google.com/', u'张朝阳'),
+    ('Sina', 'http://www.sina.com.cn/', u'王志东'),
+    ('163', 'http://www.163.com/', u'丁磊')
+]
+
+Website = namedtuple('Website', ['name', 'url', 'founder'])
+
+for website in websites:
+    website = Website._make(website)
+    print(website)
